e3c2798bf1f911902074f72bc9c30b560cb9583c
Krapivan <72234963+Krapivan@users.noreply.github.com>
2022-12-21 19:09:02 +0300

    Initial commit

---
+++ kurs/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace kurs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.path_list = new System.Windows.Forms.ListBox();
+            this.list = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SeaShell;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(13, 363);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SeaShell;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(221, 363);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 62);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // path_list
+            // 
+            this.path_list.FormattingEnabled = true;
+            this.path_list.ItemHeight = 16;
+            this.path_list.Location = new System.Drawing.Point(13, 13);
+            this.path_list.Margin = new System.Windows.Forms.Padding(4);
+            this.path_list.Name = "path_list";
+            this.path_list.Size = new System.Drawing.Size(341, 340);
+            this.path_list.TabIndex = 4;
+            this.path_list.Visible = false;
+            // 
+            // list
+            // 
+            this.list.BackColor = System.Drawing.Color.PapayaWhip;
+            this.list.FormattingEnabled = true;
+            this.list.ItemHeight = 16;
+            this.list.Location = new System.Drawing.Point(13, 13);
+            this.list.Margin = new System.Windows.Forms.Padding(4);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(341, 340);
+            this.list.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.SeaShell;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Consolas", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(153, 362);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 62);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(367, 439);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.list);
+            this.Controls.Add(this.path_list);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoLoader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox path_list;
+        private System.Windows.Forms.ListBox list;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ kurs/Form1.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kurs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                path_list.Items.Add(file);
+
+                string f_list_name = file_name_for_list(file);
+                list.Items.Add(f_list_name);
+
+                string f_name = file_name_for_auto(file);
+
+                Add_Auto(file, f_name);
+
+                Txt_Save();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int id = list.SelectedIndex;
+            if (id >= 0)
+            {
+                string file = path_list.Items[id].ToString();
+                string f_name = file_name_for_auto(file);
+                Del_Auto(f_name);
+
+                path_list.Items.RemoveAt(id);
+                list.Items.RemoveAt(id);
+
+                Txt_Save();
+            }
+        }
+
+        string file_name_for_auto(string file)
+        {
+            string f_name = "";
+
+            for (int i = 0; i < file.Length; i++)
+            {
+                if (file[i] == '.')
+                {
+                    return f_name;
+                }
+                f_name += file[i];
+                if (file[i] == '\\')
+                {
+                    f_name = "";
+                }
+            }
+
+            return f_name;
+        }
+        string file_name_for_list(string file)
+        {
+            string f_name = "";
+
+            for (int i = 0; i < file.Length; i++)
+            {
+                f_name += file[i];
+                if (file[i] == '\\')
+                {
+                    f_name = "";
+                }
+            }
+
+            return f_name;
+        }
+
+        void Add_Auto(string f_path, string f_name)
+        {
+            var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+
+            key.SetValue(f_name, f_path);
+            key.Close();
+        }
+        void Del_Auto(string f_name)
+        {
+            var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+
+            key.DeleteValue(f_name, false);
+            key.Close();
+        }
+
+        void Txt_Save()
+        {
+            using (StreamWriter writer = new StreamWriter("save_path.txt", false))
+            {
+                for (int i = 0; i < path_list.Items.Count; i++)
+                {
+                    writer.WriteLineAsync(path_list.Items[i].ToString());
+                }
+            }
+            using (StreamWriter writer = new StreamWriter("save_name.txt", false))
+            {
+                for (int i = 0; i < list.Items.Count; i++)
+                {
+                    writer.WriteLineAsync(list.Items[i].ToString());
+                }
+            }
+        }
+        void Txt_Load()
+        {
+            if (File.Exists("save_path.txt"))
+            {
+                using (StreamReader reader = new StreamReader("save_path.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        path_list.Items.Add(line);
+                    }
+                }
+                using (StreamReader reader = new StreamReader("save_name.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        list.Items.Add(line);
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Txt_Load();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("!Рекомендуется выбирать файлы без точки в названии, не учит точку перед форматом файла");
+        }
+    }
+}
